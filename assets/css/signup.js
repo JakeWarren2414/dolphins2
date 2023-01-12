8ed7bd04879688e21b3512fb199c8a9fa0c0865d
Jw95z <michaelmichael6909@gmail.com>
2023-01-12 13:52:17 -0800

    Changes to be committed: 	modified:   assets/css/signup.js  Changes not staged for commit: 	modified:   assets/css/bmi.js 	modified:   assets/css/login.js 	modified:   bmi.md 	modified:   signup.md  Untracked files: 	assets/js/

--- assets/css/signup.js
+++ assets/css/signup.js
@@ -1,3 +1,30 @@
-function signUP() {
-    
+alert("connect");
+let index = {
+    init: function () {
+        $("signUPbutton").on("click", ()=>{
+            alert("execute");
+            this.save();
+        })
+    },
+    save: function () {
+        alert("connected")
+        let data = {
+            username: $("username").val(),
+            password: $("password").val()
+        };
+        $.ajax({
+            type: "POST",
+            url: "/user/",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            dataType: "json"
+        }).done(function (answer) {
+            alert("success on sign up");
+            location.href = "https://jakewarren2414.github.io/FirstFastpages/";
+        }).fail(function (error) {
+            alert("you failed");
+            alert(JSON.stringify(error))
+        })
+    }
 }
+index.init();
