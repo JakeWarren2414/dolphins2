54433471180e2e06ef0f6a33beb582998a386491
Jw95z <michaelmichael6909@gmail.com>
2023-01-15 18:39:13 -0800

    Changes to be committed: 	new file:   assets/js/signup.js 	deleted:    package.json 	deleted:    signup.js 	modified:   signup.md

---
+++ assets/js/signup.js
@@ -0,0 +1,8 @@
+
+function signup(){
+    const signupData = {
+        username: document.getElementById("#username").innerHTML,
+        password : document.getElementById("#password").innerHTML
+    };
+    console.log(signupData);
+}
